2bef2c840e877b57ac56ef0eeeb1451091266a28
Liyuzhao <YuzhaoLi@tmgworldwide.com>
2019-12-09 14:42:29 +0800

    新增容器安装supervisord参数

--- php/sh/debian.sh
+++ php/sh/debian.sh
@@ -293,9 +293,9 @@ if [ -z "${PHP_EXTENSIONS##*,swoole,*}" ]; then
     echo "---------- Install swoole(${PHP_SWOOLE_VERSION})----------"
     cd "${EXTENSIONS_PATH}"
     cp -r /usr/include/openssl "${EXTENSIONS_PATH}/openssl/include"
-    ls -l "${EXTENSIONS_PATH}/openssl/include"
     mkdir swoole
     tar -zxvf "${PHP_SWOOLE_VERSION}" -C swoole --strip-components=1
+    ls -l "${EXTENSIONS_PATH}/openssl/include"
     cd swoole && phpize && ./configure --enable-openssl --with-openssl-dir="${EXTENSIONS_PATH}" && make ${MC} && make install
     docker-php-ext-enable swoole
 fi
