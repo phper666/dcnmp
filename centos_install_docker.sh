309b37063df5b3164342928aa939e12fade48f7a
liyuzhao <8603583yuPENG>
2020-03-03 15:52:38 +0800

    新增快速安装docker和docker-compose脚本

---
+++ centos_install_docker.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+DOCKER_COMPOSE_VERSION="1.25.4"
+
+echo "Install Docker Start"
+
+# yum更新包
+yum -y update
+
+# 卸载旧的docker
+yum -y remove docker docker-common docker-selinux docker-engine
+
+# 安装基础的工具yum-utils device-mapper-persistent-data lvm2
+yum -y install yum-utils device-mapper-persistent-data lvm2
+
+# 添加docker-ce yum仓库
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+# 安装docker-ce
+yum -y install docker-ce
+
+# 启动、停止docker服务
+systemctl stop docker
+systemctl start docker
+# 关闭docker服务
+# systemctl stop docker
+
+# 开机自启动docker
+systemctl enable docker
+
+echo "Docker Version"
+docker version
+
+echo "Install Dcoker-compose"
+echo '{ "registry-mirrors": ["http://docker.mirrors.ustc.edu.cn","http://hub-mirror.c.163.com","registry.docker-cn.com"] }' > /etc/docker/daemon.json
+
+# 获取docker-compose文件
+# curl -L https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+# 国内加速
+curl -L https://get.daocloud.io/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+
+# 给权限
+chmod +x /usr/local/bin/docker-compose
+
+echo "Docker-compose Version"
+docker-compose version
